52df88ff19b1106eab4e76314abf4d434e5f4052
gnembhard <gnembbhard2020@fau.edu>
2025-11-21 23:41:17 -0500

    Initial Commit

---
+++ TranslateApp/TranslateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateAppApp.swift
+//  TranslateApp
+//
+//  Created by Giovanni Nembhard on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct TranslateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TranslateAppTests/TranslateAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateAppTests.swift
+//  TranslateAppTests
+//
+//  Created by Giovanni Nembhard on 11/21/25.
+//
+
+import Testing
+@testable import TranslateApp
+
+struct TranslateAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
